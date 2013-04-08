f92154b1988e05d46dd3fd01ddf2e4ab6f543d8b
nathanlaan <nathanlaan@gmail.com>
2013-04-07 21:16:07 -0600

    Adding site framework.

---
+++ ListKeep.Web/js/app.js
@@ -0,0 +1,104 @@
+﻿$(function () {
+
+    var defaultTemplate_name = 'DefaultTemplate.html';
+    var listTemplate_name = 'ListTemplate.html';
+    var templatesDir = '/jstemplates/';
+
+    //
+    // load templates
+    //
+    underloader.loadTemplates(templatesDir, [defaultTemplate_name, listTemplate_name], function () {
+
+        ListKeepModel = Backbone.Model.extend({
+            initialize: function () {
+              console.log('--ListKeepModel.initialize()--');
+            },
+            addList: function (f) {
+              console.log("ListKeepModel.addList(): " + f);
+            }
+        });
+
+        DefaultView = Backbone.View.extend({
+            initialize: function () {
+                this.renderView();
+            },
+            renderView: function () {
+                var templateContent = underloader.get(defaultTemplate_name);
+                var variables = { addListLabel: "Add List" };
+                var template = _.template($("#addListTemplate", templateContent).prevObject.html(), variables);
+                this.$el.html(template);
+                return this;
+            },
+            events: {
+                "click input[type=button]": "addList_EventHandler"
+            },
+            addList_EventHandler: function (event) {
+                // event.currentTarget
+                //rfm.addFeed($("#addFeed_URL").val());
+                alert('addList_EventHandler');
+            }
+
+        });
+
+        ListView = Backbone.View.extend({
+            initialize: function () {
+                this.renderView();
+            },
+            renderView: function () {
+                var templateContent = underloader.get(listTemplate_name);
+                var variables = { listLabel: "--LIST--TITLE--" };
+                var template = _.template($("#listTemplate", templateContent).prevObject.html(), variables);
+                this.$el.html(template);
+                return this;
+            }
+        });
+
+        var lkm = new ListKeepModel();
+
+
+        var ListKeepAppRouter = Backbone.Router.extend({
+            routes: {
+                "list/:id": "listViewRoute",
+                "add": "addRoute",
+                "*actions": "defaultRoute" // matches http://listkeep.net/#anything-here
+            },
+            showView: function (selector, view) {
+                if (this.currentView)
+                    this.currentView.close();
+                $(selector).html(view.render().el);
+                this.currentView = view;
+                return view;
+            }
+        });
+
+
+        // Initiate the router
+        var lkar = new ListKeepAppRouter;
+
+        lkar.on('route:defaultRoute', function (actions) {
+            console.log('ACTIONS: ' + actions);
+            $('#pageContent').html(new DefaultView().renderView().el);
+        });
+
+        lkar.on('route:listViewRoute', function (id) {
+            console.log('--route:listViewRoute--');
+            $('#pageContent').html(new ListView().renderView().el);
+        });
+
+        // required for bookmarkable URLs
+        Backbone.history.start();
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
---
+++ ListKeep.Web/js/underloader.js
@@ -0,0 +1,41 @@
+﻿//
+// Based on:
+//
+// http://coenraets.org/blog/2012/01/backbone-js-lessons-learned-and-improved-sample-app
+//
+underloader = {
+
+    // Hash of preloaded templates for the app
+    templates: {},
+
+    // Recursively pre-load all the templates for the app.
+    // This implementation should be changed in a production environment. All the template files should be
+    // concatenated in a single file.
+    loadTemplates: function (rootPath, fileNames, callback) {
+        //console.log('--underloader.loadTemplates(): ');
+        //console.log(fileNames);
+        var ulref = this;
+
+        var loadTemplate = function (index) {
+            var name = fileNames[index];
+            console.log('--DEBUG--underloader.loadTemplates() -- Loading template: ' + name);
+            $.get(rootPath + name, function (data) {
+                ulref.templates[name] = data;
+                index++;
+                if (index < fileNames.length) {
+                    loadTemplate(index);
+                } else {
+                    callback();
+                }
+            });
+        }
+
+        loadTemplate(0);
+    },
+
+    // Get template by name from hash of preloaded templates
+    get: function (name) {
+        return this.templates[name];
+    }
+
+};
