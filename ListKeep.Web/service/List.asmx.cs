1c37c26b5438822462be252dedc996db48d2414f
nathanlaan <nathanlaan@gmail.com>
2013-04-09 21:57:03 -0600

    Added random ID generator.

--- ListKeep.Web/service/List.asmx.cs
+++ ListKeep.Web/service/List.asmx.cs
@@ -6,6 +6,8 @@ using System.Web.Script.Services;
 
 namespace ListKeep.Web.service
 {
+
+
     /// <summary>
     /// Summary description for List
     /// </summary>
@@ -15,11 +17,27 @@ namespace ListKeep.Web.service
     [System.Web.Script.Services.ScriptService]
     public class List : System.Web.Services.WebService
     {
+        private string CreateRandom()
+        {
+            var validChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            char[] charList = new char[8];
+            Random random = new Random();
+            for (int i = 0; i < charList.Length; i++)
+            {
+                charList[i] = validChars[random.Next(validChars.Length)];
+            }
+            return new String(charList);
+        }
 
         [WebMethod]
         public string CreateList(string name, string email)
         {
-            return Guid.NewGuid().ToString();
+            string id = CreateRandom();
+            //
+            // TODO: Check to make sure string does not already exist in the (non-existant) data store
+            // TODO: Save
+            //
+            return id;
         }
 
     }
